4fd29adbcdde4155d67ccbb0d4d4d96ffcb566d9
Michael Helfer <Michael.Helfer@bestbuy.com>
2012-12-27 19:45:59 -0600

    Initial Commit

---
+++ src/com/sbj/agnihotra/Agnihotra.java
@@ -0,0 +1,15 @@
+package com.sbj.agnihotra;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Agnihotra extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
